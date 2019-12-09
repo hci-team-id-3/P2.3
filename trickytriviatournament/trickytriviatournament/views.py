0ddf5919921063394048121d84bf2abd74b07395
Michael Kiros <michaelakiros@gmail.com>
2019-12-08 20:20:58 -0600

    Add results stuff

--- trickytriviatournament/trickytriviatournament/views.py
+++ trickytriviatournament/trickytriviatournament/views.py
@@ -3,6 +3,7 @@ from django.http import HttpRequest, HttpResponse
 
 from .models import Question
 
+
 def index(request: HttpRequest) -> HttpResponse:
 	"""
 	The index page, '/', renders the main_menu.html file
@@ -14,15 +15,18 @@ def index(request: HttpRequest) -> HttpResponse:
 
 	})
 
+
 def how_to_play(request: HttpRequest) -> HttpResponse:
 	return render(request, 'howtoplay.html', {
 
 	})
 
+
 def main_menu(request: HttpRequest) -> HttpResponse:
 	return render(request, 'main_menu.html', {
-	
+
 	})
+
 
 def question(request: HttpRequest, index: int = 1, correct: str = None) -> HttpResponse:
 	value = request.COOKIES.get('score')
@@ -39,6 +43,12 @@ def question(request: HttpRequest, index: int = 1, correct: str = None) -> HttpR
 			score = str(cscore + 1)
 		else:
 			score = request.COOKIES.get('score')
+
+	if index == 30:
+		response = render(request, 'results.html', {
+			'score': request.COOKIES.get('score')
+		})
+		return response
 
 	question = Question.objects.all()[index];
 	response = render(request, 'question_template.html', {
